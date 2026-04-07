2cf51f6dba6eb60462e1a1de3dd51acf278fc0ab
delaespriellaalcides-dotcom <delaespriellaalcides@gmail.com>
2026-04-07 13:46:32 -0500

    Add files via upload

---
+++ sw_js.js
@@ -0,0 +1,12 @@
+const CACHE = 'ddv-4g-v1';
+const FILES = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
